7aa7aba23e6d065119d35ece7098fb06b3e74d86
sezer <sezer.ayran@gmail.com>
2022-10-20 14:44:00 +0300

    Write interceptor method assigning to some values to properties

--- src/Infrastructure/MiniECommerce.Persistence/Contexts/MiniECommerceDbContext.cs
+++ src/Infrastructure/MiniECommerce.Persistence/Contexts/MiniECommerceDbContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.EntityFrameworkCore;
 using MiniECommerce.Domain.Entities;
+using MiniECommerce.Domain.Entities.Common;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -17,5 +18,24 @@ namespace MiniECommerce.Persistence.Contexts
         public DbSet<Product> Products { get; set; }
         public DbSet<Order> Orders { get; set; }
         public DbSet<Customer> Customers { get; set; }
+
+
+        // Entity'lerimizin bazı property'lerinin otomatik olarak değer atamasını yapmak için merkezi bir yerden müdahele edebiliriz.
+        // Böyle bir senaryo için DbContext'in SaveChanges method'ını override ederek bir nevi interceptor yazmış oluyoruz.
+        public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            var datas = ChangeTracker.Entries<BaseEntity>();
+
+            foreach (var data in datas)
+            {
+                _ = data.State switch
+                {
+                    EntityState.Added => data.Entity.CreatedDate = DateTime.UtcNow,
+                    EntityState.Modified => data.Entity.UpdatedDate = DateTime.UtcNow
+                };
+            }
+
+            return base.SaveChangesAsync(cancellationToken);
+        }
     }
 }
--- src/Infrastructure/MiniECommerce.Persistence/Migrations/20221019144646_mig-1.Designer.cs
+++ src/Infrastructure/MiniECommerce.Persistence/Migrations/20221019144646_mig-1.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MiniECommerce.Persistence.Contexts;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -11,9 +12,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MiniECommerce.Persistence.Migrations
 {
     [DbContext(typeof(MiniECommerceDbContext))]
-    partial class MiniECommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221019144646_mig-1")]
+    partial class mig1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
